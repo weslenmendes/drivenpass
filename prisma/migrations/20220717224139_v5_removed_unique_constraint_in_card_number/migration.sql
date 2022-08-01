2ee7315910beb278c17855d1068c20bec6832945
Luana Lessa <lessalsn@gmail.com>
2022-08-01 20:40:19 -0300

    Code review

--- prisma/migrations/20220717224139_v5_removed_unique_constraint_in_card_number/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "cards_number_key";
--- src/routers/initialRouter.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-
-import * as initialController from "./../controllers/initialController.js";
-
-const initialRouter = Router();
-
-initialRouter.get("/", initialController.index);
-
-export default initialRouter;
